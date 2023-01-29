a5bf0cfd4dc5474ab67e5c7f60f0ce6b7ce711a4
doruk <doruk.guler00@gmail.com>
2023-01-29 22:15:00 +0300

    Added some comments, var name changed.

--- models/yolo.py
+++ models/yolo.py
@@ -1,20 +1,16 @@
 import ultralytics
 from ultralytics import YOLO
 import cv2 as cv
-import torch
-from PIL import Image
-import torchvision.transforms as transforms
 import os
-from torchvision.utils import draw_bounding_boxes
 from utils.converter import img_to_base64
 
 
 absolute_path = os.path.dirname(__file__)
-relative_path = "best3.pt"
+relative_path = "best3.pt" # Your model weights
 full_path = os.path.join(absolute_path, relative_path)
 
 
-# Load the model
+# Load the model weights
 def load_weights():
     weights = f"{full_path}"
     return weights
@@ -24,12 +20,11 @@ def predict(img):
     results = model.predict(source=img)
 
     for result in results:
-        # convert tensor array to numpy
         bbox_arr = result.boxes.xyxy
 
-    # cv.imread(image_new.numpy())
     cv.rectangle(img, (int(bbox_arr[1][0]), int(bbox_arr[1][1])), (int(bbox_arr[1][2]), int(bbox_arr[1][3])),
                  color=(255, 0, 0), thickness=4)
-    responsed_image = img_to_base64(img)
-    return responsed_image
+
+    response_image = img_to_base64(img)
+    return response_image
 

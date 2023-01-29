66544f02d980c9ed16d1bc3e7f246bc51c4b6c33
doruk <doruk.guler00@gmail.com>
2023-01-29 22:06:37 +0300

    JSON response fixed.

--- routes/predict.py
+++ routes/predict.py
@@ -17,18 +17,12 @@ predict_router = APIRouter()
 
 @predict_router.post("/predict")
 async def predict_from_client_data(item: str = Body(...)):
-    # Decode the base64 image string into bytes
-    # image_bytes = image.decode()
-    # print(item)
     try:
         encoded = base64.b64encode(base64.b64decode(item))
         if encoded.decode() == item:
             image= base64_to_img(item)
         prediction = models.yolo.predict(image)
         #     return img_to_base64(prediction)
-        return prediction
+        return {"result" : prediction}
     except:
         return "Requested data does not the required format"
-    # Use your YOLO model to predict on the image
-    # prediction = predict(image_bytes)
-    return {"result" : predict_from_client_data}

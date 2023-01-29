2929cb01d637b43f28e046d81ed9be48f718d68e
doruk <doruk.guler00@gmail.com>
2023-01-29 18:03:05 +0300

    predict function fixed

--- models/yolo.py
+++ models/yolo.py
@@ -1,8 +1,12 @@
 import ultralytics
 from ultralytics import YOLO
 import cv2 as cv
+import torch
+from PIL import Image
+import torchvision.transforms as transforms
 import os
-
+from torchvision.utils import draw_bounding_boxes
+from utils.converter import img_to_base64
 
 
 absolute_path = os.path.dirname(__file__)
@@ -18,12 +22,14 @@ def load_weights():
 def predict(img):
     model = YOLO(load_weights())
     results = model.predict(source=img)
-    # Draw Bboxes
+
     for result in results:
         # convert tensor array to numpy
-        bbox_arr = result.boxes.xyxy.numpy()
-        cv.rectangle(img, (int(bbox_arr[1][0]), int(bbox_arr[1][1])), (int(bbox_arr[1][2]), int(bbox_arr[1][3])),
-                     color=(255, 0, 0), thickness=4)  # ,cv.COLORR[int(bbox_arr[5])], 3)
-        # cv.rectangle(p
-        # ath,((int(bbox_arr[0]),int(bbox_arr[1]))
+        bbox_arr = result.boxes.xyxy
+
+    # cv.imread(image_new.numpy())
+    cv.rectangle(img, (int(bbox_arr[1][0]), int(bbox_arr[1][1])), (int(bbox_arr[1][2]), int(bbox_arr[1][3])),
+                 color=(255, 0, 0), thickness=4)
+    responsed_image = img_to_base64(img)
+    return responsed_image
 
